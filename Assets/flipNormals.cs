16067369b18c8ef6f26eb772bcbe1d53266a9a8c
pOrtiz01 <18plog@queensu.ca>
2020-04-28 22:25:44 -0400

    Initial

---
+++ Assets/flipNormals.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipNormals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+
+        Vector3[] normals = mesh.normals;
+        for (int i = 0; i < normals.Length; i++)
+        {
+            int[] tris = mesh.GetTriangles(i);
+            for (int j = 0; j < tris.Length; j++)
+            {
+                int temp = tris[j];
+                tris[j] = tris[j + 1];
+                tris[j + 1] = temp;
+            }
+            mesh.SetTriangles(tris, i);
+        }
+    }
+
+}
